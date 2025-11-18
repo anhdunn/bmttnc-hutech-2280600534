a54beb5055021977e5269ed4b514b2f768233bec
anhdunn <tranngocanhduong0907@gmail.com>
2025-11-18 16:29:55 +0700

    [add] lab-01 ex04

---
+++ ex04/Main.py
@@ -0,0 +1,14 @@
+from QuanLySinhVien import QuanLySinhVien
+
+qlsv = QuanLySinhVien()
+while (1 == 1):
+    print("\nCHƯƠNG TRÌNH QUẢN LÝ SINH VIÊN")
+    print("***************************************************")
+    print("** 1. Thêm sinh viên.                            **")
+    print("** 2. Cập nhật thông tin sinh viên bởi ID.       **")
+    print("** 3. Xóa sinh viên bởi ID.                      **")
+    print("** 4. Tìm kiếm sinh vien theo ten.               **")
+    print("** 5. Sap xep sinh vien theo diem trung binh.    **")
+    print("** 6. Sap xep sinh vien theo diem trung binh.    **")
+
+
